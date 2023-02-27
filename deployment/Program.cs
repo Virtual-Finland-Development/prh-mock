aef697f7c987377dbe305cf8a429662842f8dc27
lassi.patanen <lassi.patanen@gofore.com>
2023-02-27 13:26:44 +0200

    Set DeleteBeforeReplace to true

--- deployment/Program.cs
+++ deployment/Program.cs
@@ -122,7 +122,7 @@ return await Deployment.RunAsync(() =>
                 $"aws lambda add-permission --function-name {lambdaFunction.Arn} --action lambda:InvokeFunctionUrl --principal '*' --function-url-auth-type NONE --statement-id FunctionUrlAllowAccess")
         }, new CustomResourceOptions
         {
-            DeleteBeforeReplace = false,
+            DeleteBeforeReplace = true,
             DependsOn = new InputList<Resource> { lambdaFunction }
         }
     );
