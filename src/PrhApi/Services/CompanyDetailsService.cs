3317755a8bbee231743ca009465a5e109c904d9f
Lassi Piironen <lsipii@kapsi.fi>
2023-03-20 19:46:47 +0200

    fix: company basic infos to match the defs even when no data

--- src/PrhApi/Services/CompanyDetailsService.cs
+++ src/PrhApi/Services/CompanyDetailsService.cs
@@ -90,16 +90,16 @@ public class CompanyDetailsService : ICompanyDetailsService
             RegistrationDate = company.CompanyDetails.FoundingDate,
             RegisteredAddress = new RegisteredAddress()
             {
-                FullAddress = company.CompanyAddress.FullAddress, // Might need to be defaulted to a non-null value
-                Thoroughfare = company.CompanyAddress.Thoroughfare,
-                LocatorDesignator = company.CompanyAddress.LocatorDesignator,
-                LocatorName = company.CompanyAddress.LocatorName,
-                AddressArea = company.CompanyAddress.AddressArea,
-                PostCode = company.CompanyAddress.PostCode,
-                PostName = company.CompanyAddress.PostName,
-                PoBox = company.CompanyAddress.PoBox,
+                FullAddress = company.CompanyAddress.FullAddress ?? "-",
+                Thoroughfare = company.CompanyAddress.Thoroughfare ?? "-",
+                LocatorDesignator = company.CompanyAddress.LocatorDesignator ?? "-",
+                LocatorName = company.CompanyAddress.LocatorName ?? "-",
+                AddressArea = company.CompanyAddress.AddressArea ?? "-",
+                PostCode = company.CompanyAddress.PostCode ?? "-",
+                PostName = company.CompanyAddress.PostName ?? "-",
+                PoBox = company.CompanyAddress.PoBox ?? "-",
                 AdminUnitLevel1 = company.CompanyAddress.AdminUnitLevel_1 ?? "FI",
-                AdminUnitLevel2 = company.CompanyAddress.AdminUnitLevel_2
+                AdminUnitLevel2 = company.CompanyAddress.AdminUnitLevel_2 ?? "-",
             },
         };
 
